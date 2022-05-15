1186fbc750ba3133f4d710696ce28b6e168dfe7c
Your Name <backol6000@gmail.com>
2022-05-15 12:53:09 +0400

    Initial

---
+++ src/main/java/com/shop/shoponsofa/ShopOnSofaApplication.java
@@ -0,0 +1,13 @@
+package com.shop.shoponsofa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopOnSofaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopOnSofaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/shoponsofa/ShopOnSofaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.shoponsofa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOnSofaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
